813a97cda6402b8917f3ac2f06021fc4c1bf1cd5
maxyee <eyaminkhan00@gmail.com>
2021-05-10 15:47:52 +0600

    done building the thumbnail component

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,12 @@
-export default function Results() {
+import Thumbnail from './Thumbnail';
+
+export default function Results({results}) {
+    console.log(results)
     return (
         <div>
-            
+            { results.map((result) => {
+                <Thumbnail key={result.id} result={result} />
+            })}
         </div>
     )
 }
