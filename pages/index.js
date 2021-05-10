62356344bc63eeac1bf04d3ac47fb56f6a5da21d
maxyee <eyaminkhan00@gmail.com>
2021-05-10 13:46:13 +0600

    successfully fetched data from IMDB website

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from 'next/head'
 //import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <div>
       <Head>
@@ -13,12 +16,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header/>
-      {/* Nav */}
       <Nav/>
-
-      {/* Results */}
+      <Results/>
+      
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const newLocal = requests[genre]?.url || requests.fetchTrending.url
+
+  const request = await fetch (
+    `https://api.themoviedb.org/3${ 
+      newLocal
+    }`
+  )
+  .then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
